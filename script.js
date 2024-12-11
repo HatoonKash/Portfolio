3bc3f770737578ff06251f5949693823d3801132
HatoonKash <123588872+HatoonKash@users.noreply.github.com>
2024-12-11 09:58:58 +0300

    First commit
    Date: 11-12-2024

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
